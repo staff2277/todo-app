90457cf5a8d99560421307827fb2fcbb6838fa64
Mustapha Osman <wnfynn@gmail.com>
2025-08-24 15:07:25 +0000

    Add global styles and layout for Next.js application
    - Created globals.css for global styles including dark mode support and font settings.
    - Implemented layout in layout.tsx to include global styles and font variables.
    - Developed a homepage in page.tsx with a responsive design, including links to documentation and deployment options.

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,19 @@
 import express from "express";
 import cors from "cors";
+
+const app = express();
+const PORT = 5000;
+
+//Handling middleware to ensure that the front end is able to talk with the backend
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "This a message that is running in the backend" });
+});
+
+//Starting the server
+app.listen(PORT, () => {
+  console.log("The server is up and running");
+});
