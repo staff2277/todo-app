38e191fb87c8db5b4bf6bba003c5b1e7821202cb
Mustapha Osman <wnfynn@gmail.com>
2025-08-24 14:42:34 +0000

    refactor: change module type to ES module and remove server.js implementation

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,2 @@
 import express from "express";
 import cors from "cors";
-
-const app = express();
-const PORT = 5000;
-
-//Handling the middleware to ensure the frontend can talk to the backend without interuptions
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  console.log("Im in the home route and this is running on the sever");
-});
-
-app.listen(PORT, () => {
-  console.log(`Server listening on PORT: ${PORT}`);
-});
