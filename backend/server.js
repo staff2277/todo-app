4c3a919921a66a088bca9ebb2e55130ff701fda3
Mustapha Osman <wnfynn@gmail.com>
2025-08-26 18:46:45 +0000

    changes made to cors link

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://todo-app-ho2w.onrender.com"],
+    origin: ["http://localhost:3000", "https://todo-app-87vt.onrender.com"],
     credentials: true,
   })
 );
